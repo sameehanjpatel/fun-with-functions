61d8b557d5f6192f16bb7658011608ffa03bcba3
Jesen Tanadi <mail@jesentanadi.com>
2019-07-03 07:46:29 -0400

    rework textContains to not use includes

--- script.js
+++ script.js
@@ -18,7 +18,41 @@ const zipUp = (arr1, arr2) => {
 };
 
 const textContains = (str1, str2) => {
-  return str1.includes(str2);
+  // The old version, just for reference:
+  // return str1.includes(str2);
+
+  // "oxbow", "ow"
+  // i = 0, k = 0
+  // o = o? yes, k = 1
+  // x = w? no, k = 0 (reset)
+  // b = o? no, k = 0
+  // o = o? yes, k = 1
+  // w = w? yes, k = 2
+  // check if k === str2.length
+  //
+  // "foop", "oo"
+  // f = o? no, k = 0
+  // o = o? yes, k = 1
+  // o = o? yes, k = 2, stop working, since k === str2.length
+  // return k === str2.length
+
+  let k = 0; // tracks str2's index
+  for (let i = 0; i < str1.length; i++) {
+    // If we already matched str2 to a subset of str1,
+    // just break and don't work on the rest of str1
+    if (k === str2.length) {
+      break;
+    }
+
+    // If we find a match between 2 letters,
+    // move k forward by 1, if not, reset k
+    if (str1[i] === str2[k]) {
+      k++;
+    } else {
+      k = 0;
+    }
+  }
+  return k === str2.length;
 };
 
 const howMany = (str, letter) => {
